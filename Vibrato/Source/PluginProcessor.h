cb91407e2cb24ede6fdc80d2cafbfe1686a2ae9f
Juan Gil <juandagilc@gmail.com>
2017-10-13 19:47:23 +0200

    Copied and renamed an existing project

---
+++ Vibrato/Source/PluginProcessor.h
@@ -0,0 +1,113 @@
+/*
+  ==============================================================================
+
+    This code is based on the contents of the book: "Audio Effects: Theory,
+    Implementation and Application" by Joshua D. Reiss and Andrew P. McPherson.
+
+    Code by Juan Gil <http://juangil.com/>.
+    Copyright (C) 2017 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginParameter.h"
+
+//==============================================================================
+
+class VibratoAudioProcessor : public AudioProcessor
+{
+public:
+    //==============================================================================
+
+    VibratoAudioProcessor();
+    ~VibratoAudioProcessor();
+
+    //==============================================================================
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void processBlock (AudioSampleBuffer&, MidiBuffer&) override;
+
+    //==============================================================================
+
+
+
+
+
+
+    //==============================================================================
+
+    void getStateInformation (MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    //==============================================================================
+
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    //==============================================================================
+
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect () const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+
+    //==============================================================================
+
+
+
+
+
+
+    //==============================================================================
+
+    AudioSampleBuffer delayBuffer;
+    int delayBufferSamples;
+    int delayBufferChannels;
+    int delayWritePosition;
+
+    //======================================
+
+    PluginParametersManager parameters;
+
+    PluginParameterLinSlider paramDelayTime;
+    PluginParameterLinSlider paramFeedback;
+    PluginParameterLinSlider paramMix;
+
+private:
+    //==============================================================================
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (VibratoAudioProcessor)
+};
