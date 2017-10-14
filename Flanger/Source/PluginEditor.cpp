6ece8dc550e29d35c41f042a3ff7b00babf41963
Juan Gil <juandagilc@gmail.com>
2017-10-14 10:20:21 +0200

    Copied and renamed an existing project

---
+++ Flanger/Source/PluginEditor.cpp
@@ -0,0 +1,199 @@
+/*
+  ==============================================================================
+
+    Code by Juan Gil <http://juangil.com/>.
+    Copyright (C) 2017 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+
+FlangerAudioProcessorEditor::FlangerAudioProcessorEditor (FlangerAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    const OwnedArray<AudioProcessorParameter>& parameters = processor.getParameters();
+    int comboBoxCounter = 0;
+
+    int editorHeight = 2 * editorMargin;
+    for (int i = 0; i < parameters.size(); ++i) {
+        if (const AudioProcessorParameterWithID* parameter =
+                dynamic_cast<AudioProcessorParameterWithID*> (parameters[i])) {
+
+            if (processor.parameters.parameterTypes[i] == "Slider") {
+                Slider* aSlider;
+                sliders.add (aSlider = new Slider());
+                aSlider->setTextValueSuffix (parameter->label);
+                aSlider->setTextBoxStyle (Slider::TextBoxLeft,
+                                          false,
+                                          sliderTextEntryBoxWidth,
+                                          sliderTextEntryBoxHeight);
+
+                SliderAttachment* aSliderAttachment;
+                sliderAttachments.add (aSliderAttachment =
+                    new SliderAttachment (processor.parameters.valueTreeState, parameter->paramID, *aSlider));
+
+                components.add (aSlider);
+                editorHeight += sliderHeight;
+            }
+
+            //======================================
+
+            else if (processor.parameters.parameterTypes[i] == "ToggleButton") {
+                ToggleButton* aButton;
+                toggles.add (aButton = new ToggleButton());
+                aButton->setToggleState (parameter->getDefaultValue(), dontSendNotification);
+
+                ButtonAttachment* aButtonAttachment;
+                buttonAttachments.add (aButtonAttachment =
+                    new ButtonAttachment (processor.parameters.valueTreeState, parameter->paramID, *aButton));
+
+                components.add (aButton);
+                editorHeight += buttonHeight;
+            }
+
+            //======================================
+
+            else if (processor.parameters.parameterTypes[i] == "ComboBox") {
+                ComboBox* aComboBox;
+                comboBoxes.add (aComboBox = new ComboBox());
+                aComboBox->setEditableText (false);
+                aComboBox->setJustificationType (Justification::left);
+                aComboBox->addItemList (processor.parameters.comboBoxItemLists[comboBoxCounter++], 1);
+
+                ComboBoxAttachment* aComboBoxAttachment;
+                comboBoxAttachments.add (aComboBoxAttachment =
+                    new ComboBoxAttachment (processor.parameters.valueTreeState, parameter->paramID, *aComboBox));
+
+                components.add (aComboBox);
+                editorHeight += comboBoxHeight;
+            }
+
+            //======================================
+
+            Label* aLabel;
+            labels.add (aLabel = new Label (parameter->name, parameter->name));
+            aLabel->attachToComponent (components.getLast(), true);
+            addAndMakeVisible (aLabel);
+
+            components.getLast()->setName (parameter->name);
+            components.getLast()->setComponentID (parameter->paramID);
+            addAndMakeVisible (components.getLast());
+        }
+    }
+
+    addAndMakeVisible (&pitchShiftLabel);
+    editorHeight += 20;
+
+    //======================================
+
+    editorHeight += components.size() * editorPadding;
+    setSize (editorWidth, editorHeight);
+    startTimer (50);
+}
+
+FlangerAudioProcessorEditor::~FlangerAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+
+void FlangerAudioProcessorEditor::paint (Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+}
+
+void FlangerAudioProcessorEditor::resized()
+{
+    Rectangle<int> r = getLocalBounds().reduced (editorMargin);
+    r = r.removeFromRight (r.getWidth() - labelWidth);
+
+    for (int i = 0; i < components.size(); ++i) {
+        if (Slider* aSlider = dynamic_cast<Slider*> (components[i]))
+            components[i]->setBounds (r.removeFromTop (sliderHeight));
+
+        if (ToggleButton* aButton = dynamic_cast<ToggleButton*> (components[i]))
+            components[i]->setBounds (r.removeFromTop (buttonHeight));
+
+        if (ComboBox* aComboBox = dynamic_cast<ComboBox*> (components[i]))
+            components[i]->setBounds (r.removeFromTop (comboBoxHeight));
+
+        r = r.removeFromBottom (r.getHeight() - editorPadding);
+    }
+
+    pitchShiftLabel.setBounds (0, getBottom() - 20, getWidth(), 20);
+}
+
+//==============================================================================
+
+void FlangerAudioProcessorEditor::timerCallback()
+{
+    updateUIcomponents();
+}
+
+void FlangerAudioProcessorEditor::updateUIcomponents()
+{
+    float minPitch = 0.0f;
+    float maxPitch = 0.0f;
+    float minSpeed = 1.0f;
+    float maxSpeed = 1.0f;
+    String pitchShiftText = "";
+
+    float width = processor.paramWidth.getTargetValue();
+    float frequency = processor.paramFrequency.getTargetValue();
+    int waveform = (int)processor.paramWaveform.getTargetValue();
+
+    switch (waveform) {
+        case FlangerAudioProcessor::waveformSine: {
+            minSpeed = 1.0f - M_PI * width * frequency;
+            maxSpeed = 1.0f + M_PI * width * frequency;
+            break;
+        }
+        case FlangerAudioProcessor::waveformTriangle: {
+            minSpeed = 1.0f - 2.0f * width * frequency;
+            maxSpeed = 1.0f + 2.0f * width * frequency;
+            break;
+        }
+        case FlangerAudioProcessor::waveformSawtooth: {
+            minSpeed = 1.0f - width * frequency;
+            maxSpeed = 1.0f;
+            break;
+        }
+        case FlangerAudioProcessor::waveformInverseSawtooth: {
+            minSpeed = 1.0f;
+            maxSpeed = 1.0f + width * frequency;
+            break;
+        }
+    }
+
+    maxPitch = 12.0f * logf (maxSpeed) / logf (2.0f);
+
+    if (minSpeed > 0.0f) {
+        minPitch = 12.0f * logf (minSpeed) / logf (2.0f);
+        pitchShiftText = String::formatted ("Vibrato range: %+.2f to %+.2f semitones (speed %.3f to %.3f)",
+                                            minPitch, maxPitch, minSpeed, maxSpeed);
+    } else {
+        pitchShiftText = String::formatted ("Vibrato range: ----- to %+.2f semitones (speed %.3f to %.3f)",
+                                            minPitch, maxPitch, minSpeed, maxSpeed);
+    }
+
+    pitchShiftLabel.setText (pitchShiftText, dontSendNotification);
+}
+
+//==============================================================================
---
+++ Flanger/Source/PluginProcessor.cpp
@@ -0,0 +1,343 @@
+/*
+  ==============================================================================
+
+    This code is based on the contents of the book: "Audio Effects: Theory,
+    Implementation and Application" by Joshua D. Reiss and Andrew P. McPherson.
+
+    Code by Juan Gil <http://juangil.com/>.
+    Copyright (C) 2017 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+#include "PluginParameter.h"
+
+//==============================================================================
+
+FlangerAudioProcessor::FlangerAudioProcessor():
+#ifndef JucePlugin_PreferredChannelConfigurations
+    AudioProcessor (BusesProperties()
+                    #if ! JucePlugin_IsMidiEffect
+                     #if ! JucePlugin_IsSynth
+                      .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                     #endif
+                      .withOutput ("Output", AudioChannelSet::stereo(), true)
+                    #endif
+                   ),
+#endif
+    parameters (*this)
+    , paramWidth (parameters, "Width", "ms", 1.0f, 50.0f, 10.0f, [](float value){ return value * 0.001f; })
+    , paramFrequency (parameters, "LFO Frequency", "Hz", 0.0f, 10.0f, 2.0f)
+    , paramWaveform (parameters, "LFO Waveform", waveformItemsUI, waveformSine)
+    , paramInterpolation (parameters, "Interpolation", interpolationItemsUI, interpolationLinear)
+{
+    parameters.valueTreeState.state = ValueTree (Identifier (getName().removeCharacters ("- ")));
+}
+
+FlangerAudioProcessor::~FlangerAudioProcessor()
+{
+}
+
+//==============================================================================
+
+void FlangerAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    const double smoothTime = 1e-3;
+    paramWidth.reset (sampleRate, smoothTime);
+    paramFrequency.reset (sampleRate, smoothTime);
+    paramWaveform.reset (sampleRate, smoothTime);
+    paramInterpolation.reset (sampleRate, smoothTime);
+
+    //======================================
+
+    float maxDelayTime = paramWidth.maxValue;
+    delayBufferSamples = (int)(maxDelayTime * (float)sampleRate) + 1;
+    if (delayBufferSamples < 1)
+        delayBufferSamples = 1;
+
+    delayBufferChannels = getTotalNumInputChannels();
+    delayBuffer.setSize (delayBufferChannels, delayBufferSamples);
+    delayBuffer.clear();
+
+    delayWritePosition = 0;
+    lfoPhase = 0.0f;
+    inverseSampleRate = 1.0f / (float)sampleRate;
+    twoPi = 2.0f * M_PI;
+}
+
+void FlangerAudioProcessor::releaseResources()
+{
+}
+
+void FlangerAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    ScopedNoDenormals noDenormals;
+
+    const int numInputChannels = getTotalNumInputChannels();
+    const int numOutputChannels = getTotalNumOutputChannels();
+    const int numSamples = buffer.getNumSamples();
+
+    //======================================
+
+    float currentWidth = paramWidth.getNextValue();
+    float currentFrequency = paramFrequency.getNextValue();
+
+    int localWritePosition;
+    float phase;
+
+    for (int channel = 0; channel < numInputChannels; ++channel) {
+        float* channelData = buffer.getWritePointer (channel);
+        float* delayData = delayBuffer.getWritePointer (channel);
+        localWritePosition = delayWritePosition;
+        phase = lfoPhase;
+
+        for (int sample = 0; sample < numSamples; ++sample) {
+            const float in = channelData[sample];
+            float out = 0.0f;
+
+            float localDelayTime = currentWidth * lfo (phase, (int)paramWaveform.getTargetValue()) * (float)getSampleRate();
+
+            float readPosition =
+                fmodf ((float)localWritePosition - localDelayTime + (float)delayBufferSamples - 1.0f, delayBufferSamples);
+            int localReadPosition = floorf (readPosition);
+
+            switch ((int)paramInterpolation.getTargetValue()) {
+                case interpolationNearestNeighbour: {
+                    float closestSample = delayData[localReadPosition % delayBufferSamples];
+                    out = closestSample;
+                    break;
+                }
+                case interpolationLinear: {
+                    float fraction = readPosition - (float)localReadPosition;
+                    float delayed0 = delayData[(localReadPosition + 0)];
+                    float delayed1 = delayData[(localReadPosition + 1) % delayBufferSamples];
+                    out = delayed0 + fraction * (delayed1 - delayed0);
+                    break;
+                }
+                case interpolationCubic: {
+                    float fraction = readPosition - (float)localReadPosition;
+                    float fractionSqrt = fraction * fraction;
+                    float fractionCube = fractionSqrt * fraction;
+
+                    float sample0 = delayData[(localReadPosition - 1 + delayBufferSamples) % delayBufferSamples];
+                    float sample1 = delayData[(localReadPosition + 0)];
+                    float sample2 = delayData[(localReadPosition + 1) % delayBufferSamples];
+                    float sample3 = delayData[(localReadPosition + 2) % delayBufferSamples];
+
+                    float a0 = - 0.5f * sample0 + 1.5f * sample1 - 1.5f * sample2 + 0.5f * sample3;
+                    float a1 = sample0 - 2.5f * sample1 + 2.0f * sample2 - 0.5f * sample3;
+                    float a2 = - 0.5f * sample0 + 0.5f * sample2;
+                    float a3 = sample1;
+                    out = a0 * fractionCube + a1 * fractionSqrt + a2 * fraction + a3;
+                    break;
+                }
+            }
+
+            channelData[sample] = out;
+            delayData[localWritePosition] = in;
+
+            if (++localWritePosition >= delayBufferSamples)
+                localWritePosition -= delayBufferSamples;
+
+            phase += currentFrequency * inverseSampleRate;
+            if (phase >= 1.0f)
+                phase -= 1.0f;
+        }
+    }
+
+    delayWritePosition = localWritePosition;
+    lfoPhase = phase;
+
+    //======================================
+
+    for (int channel = numInputChannels; channel < numOutputChannels; ++channel)
+        buffer.clear (channel, 0, numSamples);
+}
+
+//==============================================================================
+
+float FlangerAudioProcessor::lfo (float phase, int waveform)
+{
+    float out = 0.0f;
+
+    switch (waveform) {
+        case waveformSine: {
+            out = 0.5f + 0.5f * sinf (twoPi * phase);
+            break;
+        }
+        case waveformTriangle: {
+            if (phase < 0.25f)
+                out = 0.5f + 2.0f * phase;
+            else if (phase < 0.75f)
+                out = 1.0f - 2.0f * (phase - 0.25f);
+            else
+                out = 2.0f * (phase - 0.75f);
+            break;
+        }
+        case waveformSawtooth: {
+            if (phase < 0.5f)
+                out = 0.5f + phase;
+            else
+                out = phase - 0.5f;
+            break;
+        }
+        case waveformInverseSawtooth: {
+            if (phase < 0.5f)
+                out = 0.5f - phase;
+            else
+                out = 1.5f - phase;
+            break;
+        }
+    }
+
+    return out;
+}
+
+//==============================================================================
+
+
+
+
+
+
+//==============================================================================
+
+void FlangerAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    ScopedPointer<XmlElement> xml (parameters.valueTreeState.state.createXml());
+    copyXmlToBinary (*xml, destData);
+}
+
+void FlangerAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    ScopedPointer<XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+    if (xmlState != nullptr)
+        if (xmlState->hasTagName (parameters.valueTreeState.state.getType()))
+            parameters.valueTreeState.state = ValueTree::fromXml (*xmlState);
+}
+
+//==============================================================================
+
+bool FlangerAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* FlangerAudioProcessor::createEditor()
+{
+    return new FlangerAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool FlangerAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+//==============================================================================
+
+const String FlangerAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool FlangerAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool FlangerAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool FlangerAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double FlangerAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int FlangerAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int FlangerAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void FlangerAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String FlangerAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void FlangerAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new FlangerAudioProcessor();
+}
+
+//==============================================================================
