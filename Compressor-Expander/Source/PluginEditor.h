2a2643a98f49edc7560266def6999a4f17698b87
Juan Gil <juandagilc@gmail.com>
2017-10-28 12:22:23 +0200

    Copied and renamed an existing project

---
+++ Compressor-Expander/Source/PluginEditor.h
@@ -0,0 +1,81 @@
+/*
+  ==============================================================================
+
+    Code by Juan Gil <http://juangil.com/>.
+    Copyright (C) 2017 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+//==============================================================================
+
+class CompressorExpanderAudioProcessorEditor : public AudioProcessorEditor
+{
+public:
+    //==============================================================================
+
+    CompressorExpanderAudioProcessorEditor (CompressorExpanderAudioProcessor&);
+    ~CompressorExpanderAudioProcessorEditor();
+
+    //==============================================================================
+
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    //==============================================================================
+
+    CompressorExpanderAudioProcessor& processor;
+
+    enum {
+        editorWidth = 500,
+        editorMargin = 10,
+        editorPadding = 10,
+
+        sliderTextEntryBoxWidth = 100,
+        sliderTextEntryBoxHeight = 25,
+        sliderHeight = 25,
+        buttonHeight = 25,
+        comboBoxHeight = 25,
+        labelWidth = 100,
+    };
+
+    //======================================
+
+    typedef AudioProcessorValueTreeState::SliderAttachment SliderAttachment;
+    typedef AudioProcessorValueTreeState::ButtonAttachment ButtonAttachment;
+    typedef AudioProcessorValueTreeState::ComboBoxAttachment ComboBoxAttachment;
+
+    OwnedArray<SliderAttachment> sliderAttachments;
+    OwnedArray<ButtonAttachment> buttonAttachments;
+    OwnedArray<ComboBoxAttachment> comboBoxAttachments;
+
+    OwnedArray<Slider> sliders;
+    OwnedArray<ToggleButton> toggles;
+    OwnedArray<ComboBox> comboBoxes;
+
+    OwnedArray<Label> labels;
+    Array<Component*> components;
+
+    //==============================================================================
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CompressorExpanderAudioProcessorEditor)
+};
---
+++ Compressor-Expander/Source/PluginParameter.h
@@ -0,0 +1,233 @@
+/*
+  ==============================================================================
+
+    Code by Juan Gil <http://juangil.com/>.
+    Copyright (C) 2017 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+
+class PluginParametersManager
+{
+public:
+    PluginParametersManager (AudioProcessor& p) : valueTreeState (p, nullptr)
+    {
+    }
+
+    AudioProcessorValueTreeState valueTreeState;
+    StringArray parameterTypes;
+    Array<StringArray> comboBoxItemLists;
+};
+
+//==============================================================================
+
+class PluginParameter
+    : public LinearSmoothedValue<float>
+    , public AudioProcessorValueTreeState::Listener
+{
+protected:
+    PluginParameter (PluginParametersManager& parametersManager,
+                     const std::function<float (float)> callback = nullptr)
+        : parametersManager (parametersManager)
+        , callback (callback)
+    {
+    }
+
+public:
+    void updateValue (float value)
+    {
+        if (callback != nullptr)
+            setValue (callback (value));
+        else
+            setValue (value);
+    }
+
+    void parameterChanged (const String& parameterID, float newValue) override
+    {
+        updateValue (newValue);
+    }
+
+    PluginParametersManager& parametersManager;
+    std::function<float (float)> callback;
+    String paramID;
+};
+
+//==============================================================================
+
+class PluginParameterSlider : public PluginParameter
+{
+protected:
+    PluginParameterSlider (PluginParametersManager& parametersManager,
+                           const String& paramName,
+                           const String& labelText,
+                           const float minValue,
+                           const float maxValue,
+                           const float defaultValue,
+                           const std::function<float (float)> callback,
+                           const bool logarithmic)
+        : PluginParameter (parametersManager, callback)
+        , paramName (paramName)
+        , labelText (labelText)
+        , minValue (minValue)
+        , maxValue (maxValue)
+        , defaultValue (defaultValue)
+    {
+        paramID = paramName.removeCharacters (" ").toLowerCase();
+        parametersManager.parameterTypes.add ("Slider");
+
+        NormalisableRange<float> range (minValue, maxValue);
+        if (logarithmic)
+            range.setSkewForCentre (sqrt (minValue * maxValue));
+
+        parametersManager.valueTreeState.createAndAddParameter
+            (paramID, paramName, labelText, range, defaultValue,
+             [](float value){ return String (value, 2); },
+             [](const String& text){ return text.getFloatValue(); });
+
+        parametersManager.valueTreeState.addParameterListener (paramID, this);
+        updateValue (defaultValue);
+    }
+
+public:
+    const String& paramName;
+    const String& labelText;
+    const float minValue;
+    const float maxValue;
+    const float defaultValue;
+};
+
+//======================================
+
+class PluginParameterLinSlider : public PluginParameterSlider
+{
+public:
+    PluginParameterLinSlider (PluginParametersManager& parametersManager,
+                              const String& paramName,
+                              const String& labelText,
+                              const float minValue,
+                              const float maxValue,
+                              const float defaultValue,
+                              const std::function<float (float)> callback = nullptr)
+        : PluginParameterSlider (parametersManager,
+                                 paramName,
+                                 labelText,
+                                 minValue,
+                                 maxValue,
+                                 defaultValue,
+                                 callback,
+                                 false)
+    {
+    }
+};
+
+//======================================
+
+class PluginParameterLogSlider : public PluginParameterSlider
+{
+public:
+    PluginParameterLogSlider (PluginParametersManager& parametersManager,
+                              const String& paramName,
+                              const String& labelText,
+                              const float minValue,
+                              const float maxValue,
+                              const float defaultValue,
+                              const std::function<float (float)> callback = nullptr)
+        : PluginParameterSlider (parametersManager,
+                                 paramName,
+                                 labelText,
+                                 minValue,
+                                 maxValue,
+                                 defaultValue,
+                                 callback,
+                                 true)
+    {
+    }
+};
+
+//==============================================================================
+
+class PluginParameterToggle : public PluginParameter
+{
+public:
+    PluginParameterToggle (PluginParametersManager& parametersManager,
+                           const String& paramName,
+                           const bool defaultState = false,
+                           const std::function<float (float)> callback = nullptr)
+        : PluginParameter (parametersManager, callback)
+        , paramName (paramName)
+        , defaultState (defaultState)
+    {
+        paramID = paramName.removeCharacters (" ").toLowerCase();
+        parametersManager.parameterTypes.add ("ToggleButton");
+
+        const StringArray toggleStates = {"False", "True"};
+        NormalisableRange<float> range (0.0f, 1.0f, 1.0f);
+
+        parametersManager.valueTreeState.createAndAddParameter
+            (paramID, paramName, "", range, (float)defaultState,
+             [toggleStates](float value){ return toggleStates[(int)value]; },
+             [toggleStates](const String& text){ return toggleStates.indexOf (text); });
+
+        parametersManager.valueTreeState.addParameterListener (paramID, this);
+        updateValue ((float)defaultState);
+    }
+
+    const String& paramName;
+    const bool defaultState;
+};
+
+//==============================================================================
+
+class PluginParameterComboBox : public PluginParameter
+{
+public:
+    PluginParameterComboBox (PluginParametersManager& parametersManager,
+                             const String& paramName,
+                             const StringArray items,
+                             const int defaultChoice = 0,
+                             const std::function<float (const float)> callback = nullptr)
+        : PluginParameter (parametersManager, callback)
+        , paramName (paramName)
+        , items (items)
+        , defaultChoice (defaultChoice)
+    {
+        paramID = paramName.removeCharacters (" ").toLowerCase();
+        parametersManager.parameterTypes.add ("ComboBox");
+
+        parametersManager.comboBoxItemLists.add (items);
+        NormalisableRange<float> range (0.0f, (float)items.size() - 1.0f, 1.0f);
+
+        parametersManager.valueTreeState.createAndAddParameter
+            (paramID, paramName, "", range, (float)defaultChoice,
+             [items](float value){ return items[(int)value]; },
+             [items](const String& text){ return items.indexOf (text); });
+
+        parametersManager.valueTreeState.addParameterListener (paramID, this);
+        updateValue ((float)defaultChoice);
+    }
+
+    const String& paramName;
+    const StringArray items;
+    const int defaultChoice;
+};
+
+//==============================================================================
