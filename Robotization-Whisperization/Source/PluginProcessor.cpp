1a80a948e171d3fde84adfd0bcb5f695479aeedc
Juan Gil <juandagilc@gmail.com>
2017-11-01 18:37:08 +0100

    Copied and renamed an existing project

---
+++ Robotization-Whisperization/Source/PluginProcessor.cpp
@@ -0,0 +1,418 @@
+/*
+  ==============================================================================
+
+    This code is based on the contents of the book: "Audio Effects: Theory,
+    Implementation and Application" by Joshua D. Reiss and Andrew P. McPherson.
+
+    Code by Juan Gil <http://juangil.com/>.
+    Copyright (C) 2017 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+#include "PluginParameter.h"
+
+//==============================================================================
+
+RobotizationWhisperizationAudioProcessor::RobotizationWhisperizationAudioProcessor():
+#ifndef JucePlugin_PreferredChannelConfigurations
+    AudioProcessor (BusesProperties()
+                    #if ! JucePlugin_IsMidiEffect
+                     #if ! JucePlugin_IsSynth
+                      .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                     #endif
+                      .withOutput ("Output", AudioChannelSet::stereo(), true)
+                    #endif
+                   ),
+#endif
+    parameters (*this)
+    , paramFftSize (parameters, "FFT size", fftSizeItemsUI, fftSize512,
+                    [this](float value){
+                        const ScopedLock sl (lock);
+                        value = (float)(1 << ((int)value + 5));
+                        paramFftSize.setValue (value);
+                        updateFftSize();
+                        updateHopSize();
+                        updateWindow();
+                        updateWindowScaleFactor();
+                        return value;
+                    })
+    , paramHopSize (parameters, "Hop size", hopSizeItemsUI, hopSize8,
+                    [this](float value){
+                        const ScopedLock sl (lock);
+                        value = (float)(1 << ((int)value + 1));
+                        paramHopSize.setValue (value);
+                        updateFftSize();
+                        updateHopSize();
+                        updateWindow();
+                        updateWindowScaleFactor();
+                        return value;
+                    })
+    , paramWindowType (parameters, "Window type", windowTypeItemsUI, windowTypeHann,
+                       [this](float value){
+                           const ScopedLock sl (lock);
+                           paramWindowType.setValue (value);
+                           updateFftSize();
+                           updateHopSize();
+                           updateWindow();
+                           updateWindowScaleFactor();
+                           return value;
+                       })
+{
+    parameters.valueTreeState.state = ValueTree (Identifier (getName().removeCharacters ("- ")));
+}
+
+RobotizationWhisperizationAudioProcessor::~RobotizationWhisperizationAudioProcessor()
+{
+}
+
+//==============================================================================
+
+void RobotizationWhisperizationAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    const double smoothTime = 1e-3;
+    paramFftSize.reset (sampleRate, smoothTime);
+    paramHopSize.reset (sampleRate, smoothTime);
+    paramWindowType.reset (sampleRate, smoothTime);
+}
+
+void RobotizationWhisperizationAudioProcessor::releaseResources()
+{
+}
+
+void RobotizationWhisperizationAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    const ScopedLock sl (lock);
+
+    ScopedNoDenormals noDenormals;
+
+    const int numInputChannels = getTotalNumInputChannels();
+    const int numOutputChannels = getTotalNumOutputChannels();
+    const int numSamples = buffer.getNumSamples();
+
+    //======================================
+
+    int currentInputBufferWritePosition;
+    int currentOutputBufferWritePosition;
+    int currentOutputBufferReadPosition;
+    int currentSamplesSinceLastFFT;
+
+    for (int channel = 0; channel < numInputChannels; ++channel) {
+        float* channelData = buffer.getWritePointer (channel);
+
+        currentInputBufferWritePosition = inputBufferWritePosition;
+        currentOutputBufferWritePosition = outputBufferWritePosition;
+        currentOutputBufferReadPosition = outputBufferReadPosition;
+        currentSamplesSinceLastFFT = samplesSinceLastFFT;
+
+        for (int sample = 0; sample < numSamples; ++sample) {
+
+            //======================================
+
+            const float in = channelData[sample];
+            channelData[sample] = outputBuffer.getSample (channel, currentOutputBufferReadPosition);
+
+            //======================================
+
+            outputBuffer.setSample (channel, currentOutputBufferReadPosition, 0.0f);
+            if (++currentOutputBufferReadPosition >= outputBufferLength)
+                currentOutputBufferReadPosition = 0;
+
+            //======================================
+
+            inputBuffer.setSample (channel, currentInputBufferWritePosition, in);
+            if (++currentInputBufferWritePosition >= inputBufferLength)
+                currentInputBufferWritePosition = 0;
+
+            //======================================
+
+            if (++currentSamplesSinceLastFFT >= hopSize) {
+                currentSamplesSinceLastFFT = 0;
+
+                //======================================
+
+                int inputBufferIndex = currentInputBufferWritePosition;
+                for (int index = 0; index < fftSize; ++index) {
+                    fftTimeDomain[index].real (fftWindow[index] * inputBuffer.getSample (channel, inputBufferIndex));
+                    fftTimeDomain[index].imag (0.0f);
+
+                    if (++inputBufferIndex >= inputBufferLength)
+                        inputBufferIndex = 0;
+                }
+
+                //======================================
+
+                fft->perform (fftTimeDomain, fftFrequencyDomain, false);
+
+                for (int index = 0; index < fftSize / 2 + 1; ++index) {
+                    float magnitude = abs (fftFrequencyDomain[index]);
+                    float phase = arg (fftFrequencyDomain[index]);
+
+                    fftFrequencyDomain[index].real (magnitude * cosf (phase));
+                    fftFrequencyDomain[index].imag (magnitude * sinf (phase));
+                    if (index > 0 && index < fftSize / 2) {
+                        fftFrequencyDomain[fftSize - index].real (magnitude * cosf (phase));
+                        fftFrequencyDomain[fftSize - index].imag (magnitude * sinf (-phase));
+                    }
+                }
+
+                fft->perform (fftFrequencyDomain, fftTimeDomain, true);
+
+                //======================================
+
+                int outputBufferIndex = currentOutputBufferWritePosition;
+                for (int index = 0; index < fftSize; ++index) {
+                    float out = outputBuffer.getSample (channel, outputBufferIndex);
+                    out += fftTimeDomain[index].real() * windowScaleFactor;
+                    outputBuffer.setSample (channel, outputBufferIndex, out);
+
+                    if (++outputBufferIndex >= outputBufferLength)
+                        outputBufferIndex = 0;
+                }
+
+                //======================================
+
+                currentOutputBufferWritePosition += hopSize;
+                if (currentOutputBufferWritePosition >= outputBufferLength)
+                    currentOutputBufferWritePosition = 0;
+            }
+
+            //======================================
+        }
+    }
+
+    inputBufferWritePosition = currentInputBufferWritePosition;
+    outputBufferWritePosition = currentOutputBufferWritePosition;
+    outputBufferReadPosition = currentOutputBufferReadPosition;
+    samplesSinceLastFFT = currentSamplesSinceLastFFT;
+
+    //======================================
+
+    for (int channel = numInputChannels; channel < numOutputChannels; ++channel)
+        buffer.clear (channel, 0, numSamples);
+}
+
+//==============================================================================
+
+void RobotizationWhisperizationAudioProcessor::updateFftSize()
+{
+    fftSize = (int)paramFftSize.getTargetValue();
+    fft = new dsp::FFT (log2 (fftSize));
+
+    inputBufferLength = fftSize;
+    inputBufferWritePosition = 0;
+    inputBuffer.clear();
+    inputBuffer.setSize (getTotalNumInputChannels(), inputBufferLength);
+
+    outputBufferLength = fftSize;
+    outputBufferWritePosition = 0;
+    outputBufferReadPosition = 0;
+    outputBuffer.clear();
+    outputBuffer.setSize (getTotalNumInputChannels(), outputBufferLength);
+
+    fftWindow.realloc (fftSize);
+    fftWindow.clear (fftSize);
+
+    fftTimeDomain.realloc (fftSize);
+    fftTimeDomain.clear (fftSize);
+
+    fftFrequencyDomain.realloc (fftSize);
+    fftFrequencyDomain.clear (fftSize);
+
+    samplesSinceLastFFT = 0;
+}
+
+void RobotizationWhisperizationAudioProcessor::updateHopSize()
+{
+    overlap = (int)paramHopSize.getTargetValue();
+    if (overlap != 0) {
+        hopSize = fftSize / overlap;
+        outputBufferWritePosition = hopSize % outputBufferLength;
+    }
+}
+
+void RobotizationWhisperizationAudioProcessor::updateWindow()
+{
+    switch ((int)paramWindowType.getTargetValue()) {
+        case windowTypeRectangular: {
+            for (int sample = 0; sample < fftSize; ++sample)
+                fftWindow[sample] = 1.0f;
+            break;
+        }
+        case windowTypeBartlett: {
+            for (int sample = 0; sample < fftSize; ++sample)
+                fftWindow[sample] = 1.0f - fabs (2.0f * (float)sample / (float)(fftSize - 1) - 1.0f);
+            break;
+        }
+        case windowTypeHann: {
+            for (int sample = 0; sample < fftSize; ++sample)
+                fftWindow[sample] = 0.5f - 0.5f * cosf (2.0f * M_PI * (float)sample / (float)(fftSize - 1));
+            break;
+        }
+        case windowTypeHamming: {
+            for (int sample = 0; sample < fftSize; ++sample)
+                fftWindow[sample] = 0.54f - 0.46f * cosf (2.0f * M_PI * (float)sample / (float)(fftSize - 1));
+            break;
+        }
+    }
+}
+
+void RobotizationWhisperizationAudioProcessor::updateWindowScaleFactor()
+{
+    float windowSum = 0.0f;
+    for (int sample = 0; sample < fftSize; ++sample)
+        windowSum += fftWindow[sample];
+
+    windowScaleFactor = 0.0f;
+    if (overlap != 0 && windowSum != 0.0f)
+        windowScaleFactor = 1.0f / (float)overlap / windowSum * (float)fftSize;
+}
+
+//==============================================================================
+
+
+
+
+
+
+//==============================================================================
+
+void RobotizationWhisperizationAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    ScopedPointer<XmlElement> xml (parameters.valueTreeState.state.createXml());
+    copyXmlToBinary (*xml, destData);
+}
+
+void RobotizationWhisperizationAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    ScopedPointer<XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+    if (xmlState != nullptr)
+        if (xmlState->hasTagName (parameters.valueTreeState.state.getType()))
+            parameters.valueTreeState.state = ValueTree::fromXml (*xmlState);
+}
+
+//==============================================================================
+
+bool RobotizationWhisperizationAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* RobotizationWhisperizationAudioProcessor::createEditor()
+{
+    return new RobotizationWhisperizationAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool RobotizationWhisperizationAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+//==============================================================================
+
+const String RobotizationWhisperizationAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool RobotizationWhisperizationAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool RobotizationWhisperizationAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool RobotizationWhisperizationAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double RobotizationWhisperizationAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int RobotizationWhisperizationAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int RobotizationWhisperizationAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void RobotizationWhisperizationAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String RobotizationWhisperizationAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void RobotizationWhisperizationAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new RobotizationWhisperizationAudioProcessor();
+}
+
+//==============================================================================
---
+++ Robotization-Whisperization/Source/PluginProcessor.h
@@ -0,0 +1,195 @@
+/*
+  ==============================================================================
+
+    This code is based on the contents of the book: "Audio Effects: Theory,
+    Implementation and Application" by Joshua D. Reiss and Andrew P. McPherson.
+
+    Code by Juan Gil <http://juangil.com/>.
+    Copyright (C) 2017 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginParameter.h"
+
+//==============================================================================
+
+class RobotizationWhisperizationAudioProcessor : public AudioProcessor
+{
+public:
+    //==============================================================================
+
+    RobotizationWhisperizationAudioProcessor();
+    ~RobotizationWhisperizationAudioProcessor();
+
+    //==============================================================================
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void processBlock (AudioSampleBuffer&, MidiBuffer&) override;
+
+    //==============================================================================
+
+
+
+
+
+
+    //==============================================================================
+
+    void getStateInformation (MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    //==============================================================================
+
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    //==============================================================================
+
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect () const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+
+    //==============================================================================
+
+
+
+
+
+
+    //==============================================================================
+
+    StringArray fftSizeItemsUI = {
+        "32",
+        "64",
+        "128",
+        "256",
+        "512",
+        "1024",
+        "2048",
+        "4096",
+        "8192"
+    };
+
+    enum fftSizeIndex {
+        fftSize32 = 0,
+        fftSize64,
+        fftSize128,
+        fftSize256,
+        fftSize512,
+        fftSize1024,
+        fftSize2048,
+        fftSize4096,
+        fftSize8192,
+    };
+
+    //======================================
+
+    StringArray hopSizeItemsUI = {
+        "1/2 Window",
+        "1/4 Window",
+        "1/8 Window",
+    };
+
+    enum hopSizeIndex {
+        hopSize2 = 0,
+        hopSize4,
+        hopSize8,
+    };
+
+    //======================================
+
+    StringArray windowTypeItemsUI = {
+        "Rectangular",
+        "Bartlett",
+        "Hann",
+        "Hamming",
+    };
+
+    enum windowTypeIndex {
+        windowTypeRectangular = 0,
+        windowTypeBartlett,
+        windowTypeHann,
+        windowTypeHamming,
+    };
+
+    //======================================
+
+    void updateFftSize();
+    void updateHopSize();
+    void updateWindow();
+    void updateWindowScaleFactor();
+
+    //======================================
+
+    CriticalSection lock;
+
+    int fftSize;
+    ScopedPointer<dsp::FFT> fft;
+
+    int inputBufferLength;
+    int inputBufferWritePosition;
+    AudioSampleBuffer inputBuffer;
+
+    int outputBufferLength;
+    int outputBufferWritePosition;
+    int outputBufferReadPosition;
+    AudioSampleBuffer outputBuffer;
+
+    HeapBlock<float> fftWindow;
+    HeapBlock<dsp::Complex<float>> fftTimeDomain;
+    HeapBlock<dsp::Complex<float>> fftFrequencyDomain;
+
+    int samplesSinceLastFFT;
+
+    int overlap;
+    int hopSize;
+    float windowScaleFactor;
+
+    //======================================
+
+    PluginParametersManager parameters;
+
+    PluginParameterComboBox paramFftSize;
+    PluginParameterComboBox paramHopSize;
+    PluginParameterComboBox paramWindowType;
+
+private:
+    //==============================================================================
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RobotizationWhisperizationAudioProcessor)
+};
