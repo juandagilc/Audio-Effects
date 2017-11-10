9f2351934b689d453957e0289b2fa85bf4e6289a
Juan Gil <juandagilc@gmail.com>
2017-11-10 17:47:16 +0100

    Copied and renamed an existing project

---
+++ Panning/Source/PluginEditor.cpp
@@ -0,0 +1,137 @@
+/*
+  ==============================================================================
+
+    Code by Juan Gil <http://juangil.com/>.
+    Copyright (C) 2017 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+
+PanningAudioProcessorEditor::PanningAudioProcessorEditor (PanningAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    const OwnedArray<AudioProcessorParameter>& parameters = processor.getParameters();
+    int comboBoxCounter = 0;
+
+    int editorHeight = 2 * editorMargin;
+    for (int i = 0; i < parameters.size(); ++i) {
+        if (const AudioProcessorParameterWithID* parameter =
+                dynamic_cast<AudioProcessorParameterWithID*> (parameters[i])) {
+
+            if (processor.parameters.parameterTypes[i] == "Slider") {
+                Slider* aSlider;
+                sliders.add (aSlider = new Slider());
+                aSlider->setTextValueSuffix (parameter->label);
+                aSlider->setTextBoxStyle (Slider::TextBoxLeft,
+                                          false,
+                                          sliderTextEntryBoxWidth,
+                                          sliderTextEntryBoxHeight);
+
+                SliderAttachment* aSliderAttachment;
+                sliderAttachments.add (aSliderAttachment =
+                    new SliderAttachment (processor.parameters.valueTreeState, parameter->paramID, *aSlider));
+
+                components.add (aSlider);
+                editorHeight += sliderHeight;
+            }
+
+            //======================================
+
+            else if (processor.parameters.parameterTypes[i] == "ToggleButton") {
+                ToggleButton* aButton;
+                toggles.add (aButton = new ToggleButton());
+                aButton->setToggleState (parameter->getDefaultValue(), dontSendNotification);
+
+                ButtonAttachment* aButtonAttachment;
+                buttonAttachments.add (aButtonAttachment =
+                    new ButtonAttachment (processor.parameters.valueTreeState, parameter->paramID, *aButton));
+
+                components.add (aButton);
+                editorHeight += buttonHeight;
+            }
+
+            //======================================
+
+            else if (processor.parameters.parameterTypes[i] == "ComboBox") {
+                ComboBox* aComboBox;
+                comboBoxes.add (aComboBox = new ComboBox());
+                aComboBox->setEditableText (false);
+                aComboBox->setJustificationType (Justification::left);
+                aComboBox->addItemList (processor.parameters.comboBoxItemLists[comboBoxCounter++], 1);
+
+                ComboBoxAttachment* aComboBoxAttachment;
+                comboBoxAttachments.add (aComboBoxAttachment =
+                    new ComboBoxAttachment (processor.parameters.valueTreeState, parameter->paramID, *aComboBox));
+
+                components.add (aComboBox);
+                editorHeight += comboBoxHeight;
+            }
+
+            //======================================
+
+            Label* aLabel;
+            labels.add (aLabel = new Label (parameter->name, parameter->name));
+            aLabel->attachToComponent (components.getLast(), true);
+            addAndMakeVisible (aLabel);
+
+            components.getLast()->setName (parameter->name);
+            components.getLast()->setComponentID (parameter->paramID);
+            addAndMakeVisible (components.getLast());
+        }
+    }
+
+    //======================================
+
+    editorHeight += components.size() * editorPadding;
+    setSize (editorWidth, editorHeight);
+}
+
+PanningAudioProcessorEditor::~PanningAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+
+void PanningAudioProcessorEditor::paint (Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+}
+
+void PanningAudioProcessorEditor::resized()
+{
+    Rectangle<int> r = getLocalBounds().reduced (editorMargin);
+    r = r.removeFromRight (r.getWidth() - labelWidth);
+
+    for (int i = 0; i < components.size(); ++i) {
+        if (Slider* aSlider = dynamic_cast<Slider*> (components[i]))
+            components[i]->setBounds (r.removeFromTop (sliderHeight));
+
+        if (ToggleButton* aButton = dynamic_cast<ToggleButton*> (components[i]))
+            components[i]->setBounds (r.removeFromTop (buttonHeight));
+
+        if (ComboBox* aComboBox = dynamic_cast<ComboBox*> (components[i]))
+            components[i]->setBounds (r.removeFromTop (comboBoxHeight));
+
+        r = r.removeFromBottom (r.getHeight() - editorPadding);
+    }
+}
+
+//==============================================================================
